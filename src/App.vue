f2a7fff7872af4b185684fe77a3a0f4d6c1cb379
Vuong Cuong <vuongcuong.phoenix@gmail.com>
2021-02-22 11:50:24 +0700

    init: add `prettier` pacakge, it's configs, change VSCode's default formatter to use Prettier extension & formatted all files

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,24 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+  import { defineComponent } from 'vue';
+  import HelloWorld from './components/HelloWorld.vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-})
+  export default defineComponent({
+    name: 'App',
+    components: {
+      HelloWorld,
+    },
+  });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
