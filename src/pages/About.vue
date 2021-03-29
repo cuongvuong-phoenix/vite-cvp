75db64446644038488ed3939faced956526de5ba
Vuong Cuong <vuongcuong.phoenix@gmail.com>
2021-03-30 03:16:24 +0700

    feat(pages/about): add title for About page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,12 +1,17 @@
 <template>
-  <MarkdownAbout></MarkdownAbout>
+  <h1 class="text-4xl font-bold text-center">{{ t('nav.about') }}</h1>
+
+  <MarkdownAbout class="mt-8"></MarkdownAbout>
 </template>
 
 <script setup lang="ts">
   import { useHead } from '@vueuse/head';
+  import { useI18n } from 'vue-i18n';
   import MarkdownAbout from '~/components/markdown/About.md';
 
   useHead({
     title: 'Vite-VCP | About',
   });
+
+  const { t } = useI18n();
 </script>
