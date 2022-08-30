e58f208ee2af7e938f62cf5ce62a0fd4d1d418e6
Cuong Vuong Chi <cuongvuong.phoenix@gmail.com>
2022-08-30 21:50:32 +0700

    chore(types): re-generate types

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -7,6 +7,17 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    ICarbonCampsite: typeof import('~icons/carbon/campsite')['default']
+    ICodiconGithubAlt: typeof import('~icons/codicon/github-alt')['default']
+    IIonHomeOutline: typeof import('~icons/ion/home-outline')['default']
+    IIonIosBolt: typeof import('~icons/ion/ios-bolt')['default']
+    IIonIosInformationCircleOutline: typeof import('~icons/ion/ios-information-circle-outline')['default']
+    IIonLanguageOutline: typeof import('~icons/ion/language-outline')['default']
+    IIonMinus: typeof import('~icons/ion/minus')['default']
+    IIonMoonOutline: typeof import('~icons/ion/moon-outline')['default']
+    IIonPlus: typeof import('~icons/ion/plus')['default']
+    IIonSunnyOutline: typeof import('~icons/ion/sunny-outline')['default']
+    ITeenyiconsMarkdownOutline: typeof import('~icons/teenyicons/markdown-outline')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
